bee9877564415326909a6be470cdd34e27588b4b
Jun Hiroe <Jun.Hiroe@gmail.com>
2015-01-24 12:33:03 +0900

    Fix code style

--- src/node.c
+++ src/node.c
@@ -31,64 +31,88 @@ strndup0(const char *s, size_t n)
   return new;
 }
 
-strm_node* strm_value_new(strm_node* v) {
+strm_node*
+strm_value_new(strm_node* v) {
   return NULL;
 }
 
-strm_node* strm_array_new() {
+strm_node*
+strm_array_new() {
   return NULL;
 }
 
-strm_node* strm_array_add(strm_node* arr, strm_node* node) {
+strm_node*
+strm_array_add(strm_node* arr, strm_node* node)
+{
   return NULL;
 }
 
-strm_node* strm_stmt_new() {
+strm_node*
+strm_stmt_new()
+{
   return NULL;
 }
 
-strm_node* strm_let_new(strm_node* var, strm_node* node) {
+strm_node*
+strm_let_new(strm_node* var, strm_node* node)
+{
   return NULL;
 }
 
-strm_node* strm_op_new(const char* op, strm_node* lhs, strm_node* rhs) {
+strm_node*
+strm_op_new(const char* op, strm_node* lhs, strm_node* rhs)
+{
   return NULL;
 }
 
-strm_node* strm_funcall_new(strm_id id, strm_node* args, strm_node* blk) {
+strm_node*
+strm_funcall_new(strm_id id, strm_node* args, strm_node* blk)
+{
   return NULL;
 }
 
-strm_node* strm_nil_value() {
+strm_node*
+strm_nil_value()
+{
   return NULL;
 }
 
-strm_node* strm_double_new(strm_double d) {
+strm_node*
+strm_double_new(strm_double d)
+{
   strm_node* node = malloc(sizeof(strm_node));
+  
   node->type = STRM_NODE_VALUE;
   node->value.t = STRM_VALUE_DOUBLE;
   node->value.v.d = d;
   return node;
 }
 
-strm_node* strm_string_new(strm_string s) {
+strm_node*
+strm_string_new(strm_string s)
+{
   strm_node* node = malloc(sizeof(strm_node));
+
   node->type = STRM_NODE_VALUE;
   node->value.t = STRM_VALUE_STRING;
   node->value.v.s = strdup0(s);
   return node;
 }
 
-strm_node* strm_string_len_new(strm_string s, size_t l) {
+strm_node*
+strm_string_len_new(strm_string s, size_t l) {
   strm_node* node = malloc(sizeof(strm_node));
+
   node->type = STRM_NODE_VALUE;
   node->value.t = STRM_VALUE_STRING;
   node->value.v.s = strndup0(s, l);
   return node;
 }
 
-strm_node* strm_ident_new(strm_id id) {
+strm_node*
+strm_ident_new(strm_id id) {
   strm_node* node = malloc(sizeof(strm_node));
+
   node->type = STRM_NODE_IDENT;
   node->value.t = STRM_VALUE_FIXNUM;
   node->value.v.id = id;
