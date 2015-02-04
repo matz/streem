a0c1709b6ab628ed537fcede80d01a6614ff357a
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2015-02-04 22:38:00 +0900

    avoid string copy from read-only C strings

--- lib/string.c
+++ lib/string.c
@@ -1,17 +1,42 @@
 #include "strm.h"
 
+#ifdef NO_ETEXT_EDATA
+
+static inline int
+ro_data_p(const char *s)
+{
+  return 0;
+}
+
+#else
+
+extern char _etext[];
+extern char __init_array_start[];
+
+static inline int
+readonly_data_p(const char *p)
+{
+  return _etext < p && p < (char*)&__init_array_start;
+}
+#endif
+
 struct strm_string*
-strm_str_new(const char *p,size_t len)
+strm_str_new(const char *p, size_t len)
 {
   struct strm_string *str;
-  char *buf;
 
   str = malloc(sizeof(struct strm_string));
-  buf = malloc(len);
-  if (p) {
-    memcpy(buf, p, len);
+  if (readonly_data_p(p)) {
+    str->ptr = p;
   }
-  str->ptr = buf;
+  else {
+    char *buf = malloc(len);
+
+    if (p) {
+      memcpy(buf, p, len);
+    }
+    str->ptr = buf;
+  }
   str->len = len;
 
   return str;
