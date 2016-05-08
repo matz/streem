ddde9a932595071ef4fd8591469c8e0c8d0d4ff6
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2016-05-08 22:34:15 +0900

    move stat related array functions to stat.c
    sum(), average(), stdev(), variance()

--- src/stat.c
+++ src/stat.c
@@ -69,6 +69,48 @@ exec_avg(strm_stream* strm, int argc, strm_value* args, strm_value* ret)
   return exec_sum_avg(strm, argc, args, ret, TRUE);
 }
 
+static int
+ary_sum_avg(strm_stream* strm, int argc, strm_value* args, strm_value* ret, int avg)
+{
+  if (argc != 1) {
+    strm_raise(strm, "wrong number of arguments");
+    return STRM_NG;
+  }
+  else {
+    strm_array values = strm_value_ary(args[0]);
+    int i, len = strm_ary_len(values);
+    strm_value* v = strm_ary_ptr(values);
+    double sum = 0;
+    double c = 0;
+
+    for (i=0; i<len; i++) {
+      double y = strm_value_flt(v[i]) - c;
+      double t = sum + y;
+      c = (t - sum) - y;
+      sum =  t;
+    }
+    if (avg) {
+      *ret = strm_flt_value(sum/len);
+    }
+    else {
+      *ret = strm_flt_value(sum);
+    }
+    return STRM_OK;
+  }
+}
+
+static int
+ary_sum(strm_stream* strm, int argc, strm_value* args, strm_value* ret)
+{
+  return ary_sum_avg(strm, argc, args, ret, FALSE);
+}
+
+static int
+ary_avg(strm_stream* strm, int argc, strm_value* args, strm_value* ret)
+{
+  return ary_sum_avg(strm, argc, args, ret, TRUE);
+}
+
 struct stdev_data {
   strm_int num;
   double s1, s2;
@@ -135,6 +177,49 @@ exec_variance(strm_stream* strm, int argc, strm_value* args, strm_value* ret)
   return exec_var_stdev(strm, argc, args, ret, FALSE);
 }
 
+static int
+ary_var_stdev(strm_stream* strm, int argc, strm_value* args, strm_value* ret, int stdev)
+{
+  if (argc != 1) {
+    strm_raise(strm, "wrong number of arguments");
+    return STRM_NG;
+  }
+  else {
+    strm_array values = strm_value_ary(args[0]);
+    int i, len = strm_ary_len(values);
+    strm_value* v = strm_ary_ptr(values);
+    double s1, s2;
+
+    s1 = s2 = 0.0;
+    for (i=0; i<len; i++) {
+      double x = strm_value_flt(v[i]);
+      x -= s1;
+      s1 += x/(i+1);
+      s2 += i * x * x / (i+1);
+    }
+    s2 = s2 / (i-1);
+    if (stdev) {
+      s2 = sqrt(s2);
+    }
+    *ret = strm_flt_value(s2);
+    return STRM_OK;
+  }
+}
+
+static int
+ary_stdev(strm_stream* strm, int argc, strm_value* args, strm_value* ret)
+{
+  return ary_var_stdev(strm, argc, args, ret, TRUE);
+}
+
+static int
+ary_var(strm_stream* strm, int argc, strm_value* args, strm_value* ret)
+{
+  return ary_var_stdev(strm, argc, args, ret, FALSE);
+}
+
+extern strm_state* strm_array_ns;
+
 void
 strm_stat_init(strm_state* state)
 {
@@ -142,4 +227,9 @@ strm_stat_init(strm_state* state)
   strm_var_def(state, "average", strm_cfunc_value(exec_avg));
   strm_var_def(state, "stdev", strm_cfunc_value(exec_stdev));
   strm_var_def(state, "variance", strm_cfunc_value(exec_variance));
+
+  strm_var_def(strm_array_ns, "sum", strm_cfunc_value(ary_sum));
+  strm_var_def(strm_array_ns, "average", strm_cfunc_value(ary_avg));
+  strm_var_def(strm_array_ns, "stdev", strm_cfunc_value(ary_stdev));
+  strm_var_def(strm_array_ns, "variance", strm_cfunc_value(ary_var));
 }
