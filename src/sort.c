e049570522e0f3158af89bb707f698a6ac427931
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2016-05-12 07:40:48 +0900

    sort.c: add stream sort

--- src/sort.c
+++ src/sort.c
@@ -80,39 +80,117 @@ mem_sort(strm_value* p, strm_int len, struct sort_arg *arg)
   }
 }
 
+struct sort_data {
+  strm_int len;
+  strm_int capa;
+  strm_value* buf;
+  strm_value func;
+};
+
+#define SORT_FIRST_CAPA 1024
+
 static int
-ary_sort(strm_stream* strm, int argc, strm_value* args, strm_value* ret)
+iter_sort(strm_stream* strm, strm_value data)
 {
-  strm_array ary;
+  struct sort_data* d = strm->data;
+
+  if (d->len >= d->capa) {
+    d->capa *= 2;
+    d->buf = realloc(d->buf, sizeof(strm_value)*d->capa);
+  }
+  d->buf[d->len++] = data;
+  return STRM_OK;
+}
+
+static int
+finish_sort(strm_stream* strm, strm_value data)
+{
+  struct sort_data* d = strm->data;
+  strm_int i, len;
+
+  if (strm_nil_p(d->func)) {
+    mem_sort(d->buf, d->len, NULL);
+  }
+  else {
+    struct sort_arg arg;
+
+    arg.strm = strm;
+    arg.func = d->func;
+    mem_sort(d->buf, d->len, &arg);
+  }
+
+  for (i=0,len=d->len; i<len; i++) {
+    strm_emit(strm, d->buf[i], NULL);
+  }
+  free(d->buf);
+  free(d);
+  return STRM_OK;
+}
+
+static int
+exec_sort(strm_stream* strm, int argc, strm_value* args, strm_value* ret)
+{
+  struct sort_data* d;
+  strm_value func;
 
   switch (argc) {
-  case 2:
+  case 0:
+    func = strm_nil_value();
+    break;
   case 1:
-    {
-      strm_value* p;
-      strm_int len;
-
-      ary = strm_ary_new(strm_ary_ptr(args[0]), strm_ary_len(args[0]));
-      p = strm_ary_ptr(ary);
-      len = strm_ary_len(ary);
-      if (argc == 1) {
-        mem_sort(p, len, NULL);
-      }
-      else {
-        struct sort_arg arg;
-
-        arg.strm = strm;
-        arg.func = args[1];
-        mem_sort(p, len, &arg);
-      }
-      *ret = strm_ary_value(ary);
-      return STRM_OK;
-    }
+    func = args[0];
     break;
   default:
     strm_raise(strm, "wrong number of arguments");
     return STRM_NG;
   }
+
+  d = malloc(sizeof(struct sort_data));
+  if (!d) return STRM_NG;
+  d->func = func;
+  d->len = 0;
+  d->capa = SORT_FIRST_CAPA;
+  d->buf = malloc(sizeof(strm_value)*SORT_FIRST_CAPA);
+  if (!d->buf) {
+    free(d);
+    return STRM_NG;
+  }
+  *ret = strm_stream_value(strm_stream_new(strm_filter, iter_sort,
+                                           finish_sort, (void*)d));
+  return STRM_OK;
+}
+
+static int
+ary_sort(strm_stream* strm, int argc, strm_value* args, strm_value* ret)
+{
+  strm_array ary;
+  strm_value* p;
+  strm_int len;
+
+  switch (argc) {
+  case 1:
+  case 2:
+    break;
+  default:
+    strm_raise(strm, "wrong number of arguments");
+    return STRM_NG;
+  }
+
+  ary = strm_ary_new(strm_ary_ptr(args[0]), strm_ary_len(args[0]));
+  p = strm_ary_ptr(ary);
+  len = strm_ary_len(ary);
+  if (argc == 1) {
+    mem_sort(p, len, NULL);
+  }
+  else {
+    struct sort_arg arg;
+
+    arg.strm = strm;
+    arg.func = args[1];
+    mem_sort(p, len, &arg);
+  }
+  *ret = strm_ary_value(ary);
+  return STRM_OK;
 }
 
 static int
@@ -146,4 +224,5 @@ strm_sort_init(strm_state* state)
 {
   strm_var_def(strm_array_ns, "sort", strm_cfunc_value(ary_sort));
   strm_var_def(state, "cmp", strm_cfunc_value(num_cmp));
+  strm_var_def(state, "sort", strm_cfunc_value(exec_sort));
 }
