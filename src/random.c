3e90a2f99c2dde329b7117d13f3482a2bef1b6c4
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2016-08-30 10:26:25 +0900

    use uint32_t xorshift128() as PRNG

--- src/random.c
+++ src/random.c
@@ -4,11 +4,13 @@
 #include <sys/time.h>
 #include <fcntl.h>
 
+
 static void
-xorshift128init(uint64_t seed[2])
+xorshift128init(uint32_t seed[4])
 {
   struct timeval tv;
-  uint64_t x;
+  uint32_t y;
+  int i;
 
 #if defined(__linux__) || defined(__FreeBSD__) || defined(__APPLE__)
   int fd = open("/dev/urandom",
@@ -32,32 +34,38 @@ xorshift128init(uint64_t seed[2])
   }
 #endif
 
-  x = 88172645463325252ULL;
+  y = 2463534242;
   gettimeofday(&tv, NULL);
-  x ^= (uint64_t)tv.tv_usec;
-  x = x ^ (x << 13);
-  x = x ^ (x >> 7);
-  x = x ^ (x << 17);
-  seed[0] = x;
-  x = x ^ (x << 13);
-  x = x ^ (x >> 7);
-  x = x ^ (x << 17);
-  seed[1] = x;
+  y ^= (uint32_t)tv.tv_usec;
+  for (i=0; i<4; i++) {
+    y = y ^ (y << 13); y = y ^ (y >> 17);
+    y = y ^ (y << 5);
+    seed[i] = y;
+  }
+  return;
 }
 
-static uint64_t
-xorshift128plus(uint64_t seed[2])
-{
-  uint64_t x = seed[0];
-  const uint64_t y = seed[1];
-  seed[0] = y;
-  x ^= x << 23; /* a */
-  seed[1] = x ^ y ^ (x >> 17) ^ (y >> 26); /* b, c */
-  return seed[1] + y;
+static uint32_t
+xorshift128(uint32_t seed[4])
+{ 
+  uint32_t x = seed[0];
+  uint32_t y = seed[1];
+  uint32_t z = seed[2];
+  uint32_t w = seed[3]; 
+  uint32_t t;
+ 
+  t = x ^ (x << 11);
+  x = y; y = z; z = w;
+  w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+  seed[0] = x;
+  seed[1] = y;
+  seed[2] = z;
+  seed[3] = w;
+  return w;
 }
 
 struct rand_data {
-  uint64_t seed[2];
+  uint32_t seed[4];
   strm_int limit;
 };
 
@@ -65,16 +73,9 @@ static int
 gen_rand(strm_stream* strm, strm_value data)
 {
   struct rand_data* d = strm->data;
-  uint64_t r = xorshift128plus(d->seed);
+  uint32_t r = xorshift128(d->seed);
 
   strm_emit(strm, strm_int_value(r % d->limit), gen_rand);
-  return STRM_OK;
-}
-
-static int
-fin_rand(strm_stream* strm, strm_value data)
-{
-  free(strm->data);
   return STRM_OK;
 }
 
@@ -100,14 +101,14 @@ exec_rand(strm_stream* strm, int argc, strm_value* args, strm_value* ret)
   else {
     xorshift128init(d->seed);
   }
-  *ret = strm_stream_value(strm_stream_new(strm_producer, gen_rand, fin_rand, (void*)d));
+  *ret = strm_stream_value(strm_stream_new(strm_producer, gen_rand, NULL, (void*)d));
   return STRM_OK;
 }
 
 static int
 rand_seed(strm_stream* strm, int argc, strm_value* args, strm_value* ret)
 {
-  uint64_t seed[2];
+  uint32_t seed[4];
 
   strm_get_args(strm, argc, args, "");
   xorshift128init(seed);
@@ -116,7 +117,7 @@ rand_seed(strm_stream* strm, int argc, strm_value* args, strm_value* ret)
 }
 
 struct sample_data {
-  uint64_t seed[2];
+  uint32_t seed[4];
   strm_int i;
   strm_int len;
   strm_value samples[0];
@@ -126,13 +127,13 @@ static int
 iter_sample(strm_stream* strm, strm_value data)
 {
   struct sample_data* d = strm->data;
-  uint64_t r;
+  uint32_t r;
 
   if (d->i < d->len) {
     d->samples[d->i++] = data;
     return STRM_OK;
   }
-  r = xorshift128plus(d->seed)%(d->i);
+  r = xorshift128(d->seed)%(d->i);
   if (r < d->len) {
     d->samples[r] = data;
   }
