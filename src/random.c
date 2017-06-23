777edf1a94d1825894ca4011bc1cd7d956c89793
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2017-06-24 02:59:08 +0900

    Fixed a memory leak.

--- src/random.c
+++ src/random.c
@@ -173,6 +173,7 @@ exec_rnorm(strm_stream* strm, int argc, strm_value* args, strm_value* ret)
   if (argc == 2) {
     if (len != sizeof(d->seed)) {
       strm_raise(strm, "seed size differ");
+      free(d);
       return STRM_NG;
     }
     memcpy(d->seed, s, len);
